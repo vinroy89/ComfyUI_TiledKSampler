b111c00ac38925f2d2d2c163014fe4b1afff9894
vince <32554081+vinroy89@users.noreply.github.com>
2025-03-07 21:11:45 -0500

    Fix for newer controlnet

--- nodes.py
+++ nodes.py
@@ -88,7 +88,7 @@ def slice_cnet(h, h_len, w, w_len, model:comfy.controlnet.ControlBase, img):
     if isinstance(model, comfy.controlnet.ControlLora):
         model.cond_hint = hint.float().to(model.device)
     else:
-        model.cond_hint = hint.to(model.control_model.dtype).to(model.device)
+        model.cond_hint = hint.to(model.control_model.dtype).to(model.control_model.device)
 
 def slices_T2I(h, h_len, w, w_len, model:comfy.controlnet.ControlBase, img):
     model.control_input = None
